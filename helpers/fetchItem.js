11594ab968cc63fc52811e5e8c2c4d1f3f430e7b
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-03-28 10:49:29 -0300

    adiciona requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,11 +1,10 @@
 //  busca o item pelo id
 const fetchItem = async (item) => {
   // npm run test:coverage
-  try {
+  if (!item) { throw new Error('You must provide an url'); }
     const take = await fetch(`https://api.mercadolibre.com/items/${item}`)
     .then((i) => i.json());
     return take;
-  } catch (erro) { return erro.message; }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,35 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se "fetchItem" é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchItem).toBe('function')
+  });
+
+  it('Ao chamar a função fetchItem com o argumento do item "MLB1615760527" verificar se fetch foi chamada;', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
+    expect.assertions(1);
+    const url = "https://api.mercadolibre.com/items/MLB1615760527";
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+
+  it('Verifica se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    expect.assertions(1);
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+
+  it('Verifica se ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url".', async () => {
+    try {
+      await fetchItem();
+    } catch (erro) {
+      expect(erro).toEqual(new Error('You must provide an url'));
+    }
+  });
+  // fail('Teste vazio');
 });
